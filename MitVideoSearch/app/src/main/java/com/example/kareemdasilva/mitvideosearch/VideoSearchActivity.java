e78a296c63e38d17e0ce49537be4bbfad51ea1d7
Kareem Dasilva <kdasilva@memphis.edu>
2015-09-19 16:05:35 -0400

    added video controls

--- MitVideoSearch/app/src/main/java/com/example/kareemdasilva/mitvideosearch/VideoSearchActivity.java
+++ MitVideoSearch/app/src/main/java/com/example/kareemdasilva/mitvideosearch/VideoSearchActivity.java
@@ -4,20 +4,64 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.MediaController;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnPreparedListener;
 import android.widget.VideoView;
 import android.net.Uri;
+import android.media.*;
+
+import java.util.List;
 
 
 public class VideoSearchActivity extends ActionBarActivity {
 
     public static VideoView videoPlayer;
+    public ListView timeStamps;
+    public MediaController control;
+    private int position = 0;
+
 
     //Setup video player by creating controls/video source
     public void videoSetup() {
         videoPlayer = (VideoView) findViewById(R.id.videoPlayer);
+        //Loads video to the video player
+        if (control == null) {
+
+            control= new MediaController(VideoSearchActivity.this);
+
+        }
+
+        videoPlayer.setMediaController(control);
+
         String uriPath = "android.resource://"+getPackageName()+"/raw/vacation";
         Uri uri = Uri.parse(uriPath);
         videoPlayer.setVideoURI(uri);
+        control = (MediaController) findViewById(R.id.controller);
+
+
+        timeStamps = (ListView) findViewById(R.id.timeMarks);
+       videoPlayer.requestFocus();
+
+        videoPlayer.setOnPreparedListener(new OnPreparedListener() {
+           public void onPrepared(MediaPlayer mediaPlayer) {
+
+                videoPlayer.seekTo(position);
+
+                if (position == 0) {
+
+                    videoPlay();
+
+                } else {
+
+                    //if we come from a resumed activity, video playback will be paused
+
+                    videoPause();
+                }
+            }
+        });
     }
     public void videoPlay(){
         videoPlayer.start();
@@ -32,7 +76,10 @@ public class VideoSearchActivity extends ActionBarActivity {
         setContentView(R.layout.activity_video_search);
         videoSetup();
         videoPlay();
+
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
