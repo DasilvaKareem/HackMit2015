6e27b9833baa7795d417e46d0ac80a6b979dd9e2
mhkane <mhkane@mit.edu>
2015-09-19 13:15:06 -0400

    Test

--- MitVideoSearch/app/src/main/java/com/example/kareemdasilva/mitvideosearch/VideoSearch.java
+++ MitVideoSearch/app/src/main/java/com/example/kareemdasilva/mitvideosearch/VideoSearch.java
@@ -18,6 +18,7 @@ public class VideoSearch extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_video_search, menu);
+        int id = 3;
         return true;
     }
 
